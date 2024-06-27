ca4d04d0db47f516f5d3abb8613f2f6d364a161a
PAT28WE <167641241+PAT28WE@users.noreply.github.com>
2024-06-27 09:41:31 -0400

    Add files via upload

---
+++ Task XPATH.py
@@ -0,0 +1,18 @@
+Absolute XPATH Guvi following list-/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[3]/ul/li[3]/div/a 
+                                   /html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[3]/ul/li[3]/div/a/text()
+                                   /html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[2]/div/div/div[2]/div/div[1]/button/div 
+
+
+Relative XPATH Guvi following list -//*[@id="mount_0_0_JN"]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[3]/ul/li[3]/div/a/text()
+                                    //*[@id="mount_0_0_JN"]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[2]/div/div/div[2]/div/div[1]/button/div
+
+
+Absolute XPATH Guvi follower list-/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[3]/ul/li[2]/div/a/text()
+                                 -/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[3]/ul/li[2]
+
+Relative XPATH Guvi follower list-//*[@id="mount_0_0_JN"]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[3]/ul/li[2]/div/a/text()
+                                 -//*[@id="mount_0_0_JN"]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[3]/ul/li[2]/div
+
+
+
+
